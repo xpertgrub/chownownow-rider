69c3de08044f3bc9df8b83a9ea2b7e6c4eb463e1
Xpert Grub <xpertgrub@users.noreply.github.com>
2019-08-06 15:52:53 +0000

    update

--- www/js/config.js
+++ www/js/config.js
@@ -4,6 +4,6 @@ var krms_driver_config ={
 	'ApiUrl':"https://www.chownownow.com/driver/api",		
 	'DialogDefaultTitle':"Chow Now Now Rider",
 	'mapboxToken' : 'pk.eyJ1IjoieHBlcnRncnViIiwiYSI6ImNqeXJkdHJxNDA0YWczY21rMG5nejZpcHYifQ.p5RIsTD22Uz4Oq4KDpWmLA',
-	'APIHasKey':"ehblcnrncnvifgnob3dub3dub3dyawrlcg",
+	'APIHasKey':"ef94ae67825525c39572b863296b57ae",
 	'debug': false
 };
